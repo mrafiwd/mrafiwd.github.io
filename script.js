b589ebda04e644a0571d5130c896daddebb9f6e7
mrafiwd <mrafi.wd@gmail.com>
2023-10-22 19:15:08 +0700

    apus index & style sebelumnya, lalu ganti yang baru

---
+++ script.js
@@ -0,0 +1,21 @@
+function sendMessage() {
+    var nama = document.getElementById("name").value;
+    var NRP = document.getElementById("nrp").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("numphone").value;
+    var message = document.getElementById("message").value;
+
+    if (nama === "" || NRP === "" || email === "" || phone === "" || message === "") {
+        alert("Semua Harus diisi");
+        return false;
+    }
+    if (isNaN(NRP)) {
+        alert("NRP is invalid");
+        return false;
+    }
+    if (isNaN(phone)) {
+        alert("Phone Number is invalid");
+        return false;
+    }
+    return true;
+}
